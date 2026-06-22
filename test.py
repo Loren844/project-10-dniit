c40bf0b37fac52c3121280d4c7fe7c24f1f5bb0e
Lorenzo BALADIER <79362075+Loren844@users.noreply.github.com>
2026-06-22 09:40:08 +0700

    test robot arm move

---
+++ test.py
@@ -0,0 +1,25 @@
+import snap7
+from snap7.util import set_dint, set_bool
+import time
+
+plc = snap7.client.Client()
+plc.connect('192.168.0.10', 0, 1)
+
+data = bytearray(12)
+set_dint(data, 0, 5000)
+set_dint(data, 4, 2000)
+set_dint(data, 8, 2000)
+plc.write_area(0x83, 0, 16, data)
+
+m2_byte = plc.read_area(0x83, 0, 2, 1)
+set_bool(m2_byte, 0, 4, True)
+set_bool(m2_byte, 0, 6, True)
+plc.write_area(0x83, 0, 2, m2_byte)
+
+time.sleep(1)
+
+set_bool(m2_byte, 0, 4, False)
+set_bool(m2_byte, 0, 6, False)
+plc.write_area(0x83, 0, 2, m2_byte)
+
+plc.disconnect()
